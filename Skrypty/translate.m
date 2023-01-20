eb778b4a6cc01a698bc0d0029dffc8ea2529c484
ElectroFoxx <electrofoxxgaming@gmail.com>
2023-01-20 01:49:18 +0100

    Update translate.m

--- Skrypty/translate.m
+++ Skrypty/translate.m
@@ -40,11 +40,35 @@ function finalText = tr(binaryImage)
         line = binaryImage(rows(i, 1):rows(i, 2), :);
         lineChars = mergeObjects(line);
         lineCharsImg = regionprops(lineChars, "Image");
+
+        spaceVector = [];
+        begin = 0;
+        startCounting = false;
+        set = false;
+        for x = 1:size(line, 2)
+            if sum(line(:, x)) > 0 && ~startCounting
+                startCounting = true;
+            elseif sum(line(:, x)) > 0 && startCounting && set
+                spaceVector(length(spaceVector) + 1) = x - begin;
+                set = false;
+            elseif sum(line(:, x)) == 0 && startCounting && ~set
+                begin = x;
+                set = true;
+            end
+
+        end
+        avgSpace = sum(spaceVector, "all") / length(spaceVector);
         
         for im = 1:max(lineChars, [], 'all')
             imgToCheck = 255 * to128Image(lineCharsImg(im).Image);
             finalText = finalText + string(classify(net, imgToCheck));
+            if im ~= max(lineChars, [], 'all')
+                if spaceVector(im) > avgSpace
+                    finalText = finalText + " ";
+                end
+            end
         end
+        finalText = finalText + newline;
     end
     
 end
