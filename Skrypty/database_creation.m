2b12902ad0f13f2c19665d4767885c7f5474c541
Piotr Kowalczyk <92885222+piotrkowalczyk53@users.noreply.github.com>
2023-01-20 20:27:43 +0100

    Add database and train

---
+++ Skrypty/database_creation.m
@@ -0,0 +1,120 @@
+% Lista czcionek
+fontsList = ["Arial", "Calibri", "Cambria", "Candara", "Century",...
+    "Constantia", "Corbel", "Courier New", "Georgia", "Lucida Sans Unicode",...
+    "Palatino Linotype", "Segoe UI", "Tahoma", "Times New Roman", "Trebuchet MS", "Verdana"];
+
+% Lista liter
+charsList = ["A", "Ą", "B", "C", "Ć", "D", "E", "Ę", "F", "G", "H", "I", "J", "K", "L", "Ł", "M", "N",...
+    "Ń", "O", "Ó", "P", "Q", "R", "S", "Ś", "T", "U", "V", "W", "X", "Y", "Z", "Ź", "Ż", "a", "ą", "b",...
+    "c", "ć", "d", "e", "ę", "f", "g", "h", "i", "j", "k", "l", "ł", "m", "n", "ń", "o", "ó", "p", "q",...
+    "r", "s", "ś", "t", "u", "v", "w", "x", "y", "z", "ź", "ż", ",", ".", "?", "!", "'", '"', "(", ")", "-",...
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+% Tworzenie folderu "database"
+database_folder = "database/";
+if ~exist(database_folder, 'dir')
+    mkdir(database_folder);
+end
+
+% Tworzenie folderów dla każdej litery
+leng = length(charsList);
+h = waitbar(0, 'Creating database structure', 'Name', 'Creating database');
+for i = 1:leng
+    folder_name = createName(charsList(i));
+    if ~exist(strcat(database_folder,folder_name), 'dir')
+       mkdir(strcat(database_folder,folder_name));
+    end
+    percentage = i / leng;
+    waitbar(percentage, h);
+end
+close(h)
+
+% Tworzenie obrazów dla każdej litery i czcionki
+h = waitbar(0, 'Creating database files (this may take a while...)', 'Name', 'Creating database');
+imageIterator = 1;
+for i = 1:leng
+    char = charsList(i);
+    folder_name = createName(char);
+    for j = 1:length(fontsList)
+        %wygeneruj obrazy z literą po różnych zniekształceniach
+            font = fontsList(j);
+            for iter = 1:100
+                image = randImage(char, font);
+                imageLabel = bwlabel(~imbinarize(rgb2gray(image)));
+                imageLabel(imageLabel > 0) = 1;
+                imageImage = regionprops(imageLabel, "Image");
+                image = imageImage(1).Image;
+                image = to128Image(image);
+                imwrite(image, strcat(database_folder,folder_name, int2str(imageIterator), '.png'));
+                imageIterator = imageIterator + 1;
+            end
+    end
+    percentage = i / leng;
+    waitbar(percentage, h);
+end
+close(h)
+
+%Funkcja tworząca obrazy ze zniekształconymi znakami
+function image = randImage(char, font)
+    im1 = insertText(255*ones(500,500),[0 0],char,'Font',font,'FontSize',200);
+    im2 = imcomplement(im1);
+    im3 = imrotate(im2, randi([-15, 15]), "nearest", "crop");
+    r = -pi/18 + (pi/9)*rand(1);
+    tform = affine2d([1, tan(r), 0; 0, 1, 0; 0, 0, 1]);
+    im4 = imwarp(im3,tform);
+    im6 = imcomplement(im4);
+    image = im6;
+end
+
+%Funckja tworząca odpowiednie nazwy folderów dla każdego znaku
+function folder_name = createName(char)
+    if char == ","
+        folder_name = "przecinek/";
+    elseif char == "."
+        folder_name = "kropka/";
+    elseif char == "?"
+        folder_name = "pytajnik/";
+    elseif char == "!"
+        folder_name = "wykrzyknik/";
+    elseif char == "'"
+        folder_name = "apostrof/";
+    elseif char == '"'
+        folder_name = "cudzysłów/";
+    elseif char == "("
+        folder_name = "lewy_nawias/";
+    elseif char == ")"
+        folder_name = "prawy_nawias/";
+    elseif char == "-"
+        folder_name = "łącznik/";
+    elseif isstrprop(char, 'upper')
+        folder_name = upper(char);
+        folder_name = strcat(folder_name, '/');
+    else
+        folder_name = char;
+        folder_name = strcat(folder_name, '_lower/');
+    end
+end
+
+function img128 = to128Image(image)
+    [y, x] = size(image);
+    if x > y
+        image = imresize(image, 128 / x);
+        [y, ~] = size(image);
+        toAdd = 128 - y;
+        if mod(toAdd, 2) == 0
+            image = [zeros(toAdd / 2, 128); image; zeros(toAdd / 2, 128)];
+        else
+            image = [zeros(fix(toAdd / 2), 128); image; zeros(fix(toAdd / 2) + 1, 128)];
+        end
+    else
+        image = imresize(image, 128 / y);
+        [~, x] = size(image);
+        toAdd = 128 - x;
+        if mod(toAdd, 2) == 0
+            image = [zeros(128, toAdd / 2), image, zeros(128, toAdd / 2)];
+        else
+            image = [zeros(128, fix(toAdd / 2)), image, zeros(128, fix(toAdd / 2) + 1)];
+        end
+    end
+    img128 = image;
+end
---
+++ Skrypty/train.m
@@ -0,0 +1,71 @@
+% Definiujemy folder z obrazami
+database_folder = 'database/';
+
+%Tworzymy imageDatastore z plików z folderu
+imds = imageDatastore(database_folder, 'IncludeSubfolders', true, 'LabelSource', 'foldernames');
+
+%Poprawiamy etykiety
+imds.Labels = cellstr(imds.Labels);
+imds.Labels(strcmp(imds.Labels,'wykrzyknik'))={'!'};
+imds.Labels(strcmp(imds.Labels,'pytajnik'))={'?'};
+imds.Labels(strcmp(imds.Labels,'kropka'))={'.'};
+imds.Labels(strcmp(imds.Labels,'przecinek'))={','};
+imds.Labels = cellfun(@(x) strrep(x,'apostrof',''''),imds.Labels, 'UniformOutput',false);
+imds.Labels(strcmp(imds.Labels,'cudzysłów'))={'"'};
+imds.Labels(strcmp(imds.Labels,'lewy_nawias'))={'('};
+imds.Labels(strcmp(imds.Labels,'prawy_nawias'))={')'};
+imds.Labels(strcmp(imds.Labels,'łącznik'))={'-'};
+imds.Labels = cellfun(@(x) x(1),imds.Labels,'un',0);
+imds.Labels = categorical(imds.Labels);
+
+%Podział danych na treningowe i walidacji
+numTrainFiles = 700;
+[imdsTrain,imdsValidation] = splitEachLabel(imds,numTrainFiles,'randomize');
+
+%Definiowanie architektury sieci
+layers = [
+    imageInputLayer([128 128 1])
+    
+    convolution2dLayer(3,8,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,16,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    fullyConnectedLayer(89)
+    softmaxLayer
+    classificationLayer];
+
+%Opcje treningu
+options = trainingOptions('sgdm', ...
+    'InitialLearnRate',0.01, ...
+    'MaxEpochs', 10, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',imdsValidation, ...
+    'ValidationFrequency',50, ...
+    'Verbose',false, ...
+    'Plots','training-progress', ...
+    'ExecutionEnvironment','gpu');
+
+
+%Trenowanie
+net = trainNetwork(imdsTrain,layers,options);
+
+
+%Sprawdzenie
+YPred = classify(net,imdsValidation);
+YValidation = imdsValidation.Labels;
+
+accuracy = sum(YPred == YValidation)/numel(YValidation)
+
+save net;
